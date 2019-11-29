c6917a8a2fa9142838d1b836b2524dbdd21e4c90
Mikael Hallberg <mikael@hallberg.com>
2019-11-29 19:36:54 +0100

    Scraped title from horoscope site.

--- src/main/java/com/hallberg/Horoscope.java
+++ src/main/java/com/hallberg/Horoscope.java
@@ -1,8 +1,13 @@
 package com.hallberg;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Horoscope {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String signInput;
         String sign = "";
@@ -15,10 +20,15 @@ public class Horoscope {
             sign = signPicker.select(signInput);
         }
 
-        if(!sign.equals("error") && args.length == 1)
-            System.out.println("https://www.sunsigns.org/" + sign + "-november-2019-horoscope/");
-        else
+        if(!sign.equals("error") && args.length == 1) {
+            String url = "https://www.sunsigns.org/" + sign + "-november-2019-horoscope/";
+            Document doc = Jsoup.connect(url).get();
+            String title = doc.title();
+            System.out.println(title);
+        }
+        else {
             System.out.println("Usage: \"Horoscope your-sign\" i. e. \"Horoscope Leo\"");
-        // https://www.sunsigns.org/sagittarius-november-2019-horoscope/
+            // https://www.sunsigns.org/sagittarius-november-2019-horoscope/
+        }
     }
 }
